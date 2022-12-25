8ea5e95b501e788d2770c879819f2146c94f7947
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-25 13:19:51 +0530

    user can see and update status

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -22,21 +22,32 @@ class Feed extends Component {
   }
 
   componentDidMount() {
-    // fetch('http://localhost:8080/auth/status', {
-    //   headers: {
-    //     Authorization: 'Bearer ' + this.props.token,
-    //   },
-    // })
-    //   .then((res) => {
-    //     if (res.status !== 200) {
-    //       throw new Error('Failed to fetch user status.')
-    //     }
-    //     return res.json()
-    //   })
-    //   .then((resData) => {
-    //     this.setState({ status: resData.status })
-    //   })
-    //   .catch(this.catchError)
+    const graphqlQuery = {
+      query: `{
+        user {
+          status
+        }
+      }
+      `,
+    }
+    fetch('http://localhost:8080/graphql', {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + this.props.token,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(graphqlQuery),
+    })
+      .then((res) => {
+        return res.json()
+      })
+      .then((resData) => {
+        if (resData.errors) {
+          throw new Error('Fetching Status Failed!')
+        }
+        this.setState({ status: resData.data.user.status })
+      })
+      .catch(this.catchError)
 
     this.loadPosts()
   }
@@ -105,24 +116,31 @@ class Feed extends Component {
 
   statusUpdateHandler = (event) => {
     event.preventDefault()
-    fetch('http://localhost:8080/auth/status', {
-      method: 'PATCH',
+    const graphqlQuery = {
+      query: `
+        mutation {
+          updateStatus(status: "${this.state.status}") {
+            status
+          }
+        }
+      `,
+    }
+    fetch('http://localhost:8080/graphql', {
+      method: 'POST',
       headers: {
         Authorization: 'Bearer ' + this.props.token,
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        status: this.state.status,
-      }),
+      body: JSON.stringify(graphqlQuery),
     })
       .then((res) => {
-        if (res.status !== 200 && res.status !== 201) {
-          throw new Error("Can't update status!")
-        }
         return res.json()
       })
       .then((resData) => {
         console.log(resData)
+        if (resData.errors) {
+          throw new Error('Updating Status Failed!')
+        }
       })
       .catch(this.catchError)
   }
